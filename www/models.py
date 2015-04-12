50877b29993e20cfe65f38320fbe7483b334dcdb
mangu93 <adrianmarinportillo@gmail.com>
2015-04-12 21:48:51 +0200

    Modelo de curso

--- www/models.py
+++ www/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Curso(models.Model):
+    name = models.CharField(max_length=50)
+    pub_date = models.DateField()
+    date_activity = models.DateField()
+    location = models.CharField(max_length=10)
+    information = models.CharField(max_length=200)
+
+class Usuario(models.Model):
+    name = models.CharField(max_length=20)
+
