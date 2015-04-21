9a546d5fe319d854214be7c13affbfbb04acca23
Angajime <tarazaky@alu.uma.es>
2015-04-21 13:50:06 +0200

    Fixed models to match intended design. First test done.)

--- www/models.py
+++ www/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Grupo(models.Model):
+#   cursos = models.ForeignKey(Curso)
+    name = models.CharField(max_length=50)
+    pub_date = models.DateField()
+
+
 class Curso(models.Model):
     name = models.CharField(max_length=50)
     pub_date = models.DateField()
     date_activity = models.DateField()
     location = models.CharField(max_length=10)
     information = models.CharField(max_length=200)
+    grupo = models.ForeignKey(Grupo)
 
-    def __str__(self):
+    def __unicode__(self):
         return "%s %s %s" % (self.name, self.date_activity, self.location)
 
+# TODO: Deberia usarse django.contrib.auth.models.User
 class Usuario(models.Model):
     name = models.CharField(max_length=20)
-
-class Grupo(models.Model):
-    # curso = models.ForeignKey(Curso)
-    name = models.CharField(max_length=50)
-
