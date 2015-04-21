5792ef7996dbb19b146240525ee27574f1d4fc6f
Francisco Baena <soy@franbaena.com>
2015-04-21 14:11:41 +0100

    I've supposed we're going to need an UserProfile model to store all information related to the user (course enrolment, extra fields for user profile, etc)…

--- www/models.py
+++ www/models.py
@@ -1,14 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class UserProfile(models.Model):
+    """
+    Datos adicionales para un usuario
+    """
+    user = models.OneToOneField(User)
+    # Any extra data
+
+    def __unicode__(self):
+        return "<UserProfile: %s>" % (self.user.username)
+
+
 class Grupo(models.Model):
+    """
+    Grupo (?) 
+    """
 #   cursos = models.ForeignKey(Curso)
     name = models.CharField(max_length=50)
     pub_date = models.DateField()
 
 
 class Curso(models.Model):
+    """
+    Curso (?)
+    """
     name = models.CharField(max_length=50)
     pub_date = models.DateField()
     date_activity = models.DateField()
@@ -18,7 +36,3 @@ class Curso(models.Model):
 
     def __unicode__(self):
         return "%s %s %s" % (self.name, self.date_activity, self.location)
-
-# TODO: Deberia usarse django.contrib.auth.models.User
-class Usuario(models.Model):
-    name = models.CharField(max_length=20)
