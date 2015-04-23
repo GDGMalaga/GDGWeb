e2ed2fb9dbf75f5cafb7362e50d52f31615987df
mangu93 <adrianmarinportillo@gmail.com>
2015-04-23 14:01:48 +0200

    Added the settings and the url needed

--- GDGWeb/settings.py
+++ GDGWeb/settings.py
@@ -26,18 +26,48 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    
+    # Required by `allauth` template tags
+    'django.core.context_processors.request',
+    'django.contrib.auth.context_processors.auth',
+        
+    # `allauth` specific context processors
+    'allauth.account.context_processors.account',
+    'allauth.socialaccount.context_processors.socialaccount',
 
+)
+
+AUTHENTICATION_BACKENDS = (
+
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+    
+)
 # Application definition
 
 INSTALLED_APPS = (
+
+    # Here comes the old ones
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # The Django sites framework is required
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
     'www'
 )
+SITE_ID = 1
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -80,6 +110,7 @@ USE_TZ = True
 
 # Required for editing the allauth templates
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+
 # Required for centralized Statifiles Dir
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"),)
 
--- GDGWeb/urls.py
+++ GDGWeb/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('allauth.urls')),
     url(r'', include('www.urls')),
 )
