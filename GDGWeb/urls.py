fe975bf57b075e41760770b256c3fd2f3d547559
Francisco Baena <soy@franbaena.com>
2015-04-24 11:40:43 +0100

    Added allauth urls

--- GDGWeb/urls.py
+++ GDGWeb/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
                        # url(r'^$', 'GDGWeb.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
                        url(r'^admin/', include(admin.site.urls)),
+                       url(r'^accounts/', include('allauth.urls')),
                        url(r'', include('www.urls')),
                        )
-
