b5fde04427a2c0600b890b5ac3f92867d345d761
Francisco Baena <soy@franbaena.com>
2015-04-24 01:57:42 +0100

    Moved home content from «base.html» to «home.html»

--- GDGWeb/urls.py
+++ GDGWeb/urls.py
@@ -5,7 +5,6 @@ urlpatterns = patterns('',
                        # Examples:
                        # url(r'^$', 'GDGWeb.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
-
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'', include('www.urls')),
-                       )
+                    )
--- www/urls.py
+++ www/urls.py
@@ -5,11 +5,8 @@ from . import views
 urlpatterns = patterns('',
                        url(r'^$', views.home, name='home'),
                        url(r'^login', views.login, name='login'),
-                       url(r'^logout', 'django.contrib.auth.views.logout',
-                           {'next_page': '/'}),
-                       url(r'^calendario',
-                           views.calendario,
-                           name='calendario'),
+                       url(r'^logout', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+                       url(r'^calendario', views.calendario, name='calendario'),
                        url(r'^grupos', views.grupos, name='grupos'),
                        url(r'^accede', views.accede, name='accede')
-                       )
+)
