68db5d5267c5c9c5acab05eda7b63a939e505269
Francisco Baena <soy@franbaena.com>
2015-04-24 02:51:29 +0100

    Sketched a little «design»

--- www/urls.py
+++ www/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.home, name='home'),
-                       url(r'^login', views.login, name='login'),
-                       url(r'^logout', 'django.contrib.auth.views.logout', {'next_page': '/'}),
-                       url(r'^calendario', views.calendario, name='calendario'),
-                       url(r'^grupos', views.grupos, name='grupos'),
-                       url(r'^accede', views.accede, name='accede')
+                       url(r'^calendario/', views.calendario, name='calendario'),
+                       url(r'^grupos/', views.grupos, name='grupos'),
 )
